23554e505b6acae5338d650b4a39eb676e420f12
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-02-13 23:45:47 +0200

    finished for today - app is working nicely and is more organized. i increased button size, made border between buttons and added placeholders for top and bottom menu's + played around a bit with styles

--- scouter_app/App.js
+++ scouter_app/App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, Alert } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	Button,
+	Alert,
+	TouchableOpacity
+} from 'react-native';
 
 function MyComp(props) {
 	return <SafeAreaView><Text>{props.txt}</Text></SafeAreaView>
@@ -17,7 +25,11 @@ export default function App() {
 	};
 
 	buttonSubBallsIn = () => {
-		setBallsIn(ballsIn - 1);
+		let res = ballsIn - 1
+		if (res < 0) {
+			res = 0
+		}
+		setBallsIn(res);
 	};
 
 	buttonAdderBallsMissed = () => {
@@ -25,37 +37,60 @@ export default function App() {
 	};
 
 	buttonSubBallsMissed = () => {
-		setBallsMissed(ballsMissed - 1);
+		let res = ballsMissed - 1
+		if (res < 0) {
+			res = 0
+		}
+		setBallsMissed(res);
 	};
 	return (
 		<SafeAreaView style={styles.container}>
+			<View style={styles.topMenu}><Text style={styles.topMenu}>top menu</Text></View>
+
 			<View style={styles.top}>
 				<Text style={styles.header}>Balls In: {ballsIn}</Text>
 				<View style={{ flex: 1, flexDirection: 'row', }}>
-					<Button style={{ flex: 3, }}
+					<TouchableOpacity
+						style={styles.button}
 						onPress={buttonAdderBallsIn}
-						title='+'
-					/>
-					<Button style={{ flex: 1, }}
+					>
+						<Text style={styles.bigText}>+</Text>
+					</TouchableOpacity>
+
+					<View style={styles.border}></View>
+
+					<TouchableOpacity
+						style={styles.button}
 						onPress={buttonSubBallsIn}
-						title='-'
-					/>
+					>
+						<Text style={styles.bigText}>-</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
 
 			<View style={styles.bottom}>
 				<Text style={styles.header}>Balls Missed: {ballsMissed}</Text>
 				<View style={{ flex: 1, flexDirection: 'row', }}>
-					<Button style={{ flex: 3, }}
+					<TouchableOpacity
+						style={styles.button}
 						onPress={buttonAdderBallsMissed}
-						title='+'
-					/>
-					<Button style={{ flex: 1, }}
+					>
+						<Text style={styles.bigText}>+</Text>
+					</TouchableOpacity>
+
+					<View style={styles.border}></View>
+
+					<TouchableOpacity
+						style={styles.button}
 						onPress={buttonSubBallsMissed}
-						title='-'
-					/>
+					>
+						<Text style={styles.bigText}>-</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
+
+			<View style={styles.bottomMenu}><Text style={styles.bottomMenu}>bottom menu</Text></View>
+
 		</SafeAreaView >
 	);
 
@@ -78,26 +113,60 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		textAlignVertical: "center",
 		fontWeight: 'bold',
-		fontSize: 20,
+		fontSize: 25,
 		marginTop: 0,
 	},
-	button: {
+	topMenu: {
 		flex: 1,
-		backgroundColor: '#00f',
-		alignItems: 'center',
+		width: '100%',
+		backgroundColor: '#aaa',
+		alignItems: 'flex-end',
 		justifyContent: 'flex-start',
+		textAlign: 'center',
+		textAlignVertical: "center",
+		fontWeight: 'bold',
+		fontSize: 25,
+		marginTop: 0,
+	},
+	bottomMenu: {
+		flex: 1,
+		width: '100%',
+		backgroundColor: '#aaa',
+		alignItems: 'flex-end',
+		justifyContent: 'flex-start',
+		textAlign: 'center',
+		textAlignVertical: "center",
+		fontWeight: 'bold',
+		fontSize: 25,
+		marginTop: 0,
+	},
+	bigText: {
+		textAlign: 'center',
+		textAlignVertical: "center",
+		fontWeight: 'bold',
+		fontSize: 100,
+	},
+	button: {
+		flex: 0.4,
+		backgroundColor: 'indigo',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	border: {
+		flex: 0.025,
+		backgroundColor: '#000',
 	},
 	top: {
 		flex: 5,
 		width: '100%',
-		backgroundColor: 'dodgerblue',
+		backgroundColor: 'ivory',
 		alignItems: 'center',
 		justifyContent: 'flex-start',
 	},
 	bottom: {
 		flex: 5,
 		width: '100%',
-		backgroundColor: 'tomato',
+		backgroundColor: 'khaki',
 		alignItems: 'center',
 		justifyContent: 'flex-start',
 	},
