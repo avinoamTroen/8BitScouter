f4ae8c94f83eaac0e9eaa25352d210d4b54af895
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-02-13 23:05:08 +0200

    changed some stuff in version control so it works, started a bit on creating the table, made a general purpose sql function in mysql_utils but didn't use it yet

--- servers/mysql_stuff/create_table.py
+++ servers/mysql_stuff/create_table.py
@@ -1,18 +1,25 @@
 import mysql.connector
 import mysql_utils
 
-mydb = mysql.connector.connect(
-    host=mysql_utils.HOST,
-    user=mysql_utils.USER,
-    password=mysql_utils.PASSWORD,
-    database=mysql_utils.DB_NAME
 
-)
+def create_tables(db):
+    """
+    will create the main tables in db
+    :param db: database (string)
+    :return: none
+    """
+    my_db = mysql.connector.connect(
+        host=mysql_utils.HOST,
+        user=mysql_utils.USER,
+        password=mysql_utils.PASSWORD,
+        database=db
 
-mycursor = mydb.cursor()
+    )
 
-query = ("CREATE TABLE customers ("
-         "id INT AUTO_INCREMENT PRIMARY KEY, "
-         "team_number VARCHAR(255), "
-         "num_of_balls INT()")
-mycursor.execute(query)
+    my_cursor = my_db.cursor()
+
+    query = ("CREATE TABLE customers ("
+             "id INT AUTO_INCREMENT PRIMARY KEY, "
+             "team_number VARCHAR(255), "
+             "num_of_balls INT())")
+    my_cursor.execute(query)
--- servers/mysql_stuff/mysql_utils.py
+++ servers/mysql_stuff/mysql_utils.py
@@ -14,6 +14,25 @@ DB_NAME = "scouting_8bit"
 
 
 # useful general purpose functions
+def exe_query(query):
+    """
+    executes query in main database
+    :param query: SQL query to execute (a string)
+    :return: my_cursor
+    """
+    my_db = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD
+    )
+
+    my_cursor = my_db.cursor()
+
+    my_cursor.execute(query)
+
+    return my_cursor
+
+
 def get_all_databases():
     my_db = mysql.connector.connect(
         host=HOST,
@@ -27,6 +46,7 @@ def get_all_databases():
     my_cursor.execute(query)
 
     list_of_dbs = []
+
     for db in my_cursor:
         list_of_dbs.append(db)
     return list_of_dbs
