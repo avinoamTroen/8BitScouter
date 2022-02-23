1961a71b437caf54ff8f6752322a627537b9e1f4
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-02-24 00:13:20 +0200

    decided what inputs will be (made word dox) and started adding them to redux. initial state is almost done, reducer and actions need to be created

--- back_end/tester_space.py
+++ back_end/tester_space.py
@@ -1,24 +1,9 @@
-txt=""" p_self = self.proto_version
+names = """names from word""".splitlines()
 
-        p_self.team_number = self.team_name
-        p_self.comp = self.comp
-        p_self.round = self.round
-        p_self.alliance_color = self.alliance
+def_vals = """vals from word""".splitlines()
 
-        p_self.intake_auto = self.auto_intake
-        p_self.bottom_auto = self.auto_bottom
-        p_self.top_auto = self.auto_top
-
-        p_self.intake_tele = self.tele_intake
-        p_self.bottom_tele = self.tele_bottom
-        p_self.top_tele = self.tele_top
-
-        p_self.climb_level = self.endgame_climbed
-
-        p_self.driving = self.driving
-        p_self.defense = self.defense
-        p_self.free_text = self.free_text"""
-
-new_txt1 = txt.replace("self", "this")
-new_txt2 = "\n".join([line + ";" for line in new_txt1.splitlines()]).replace("\n;", "\n")
-print(new_txt2)
+for name, val in zip(names, def_vals):
+    if name[0:2] != "//":
+        print(name + ": " + val + ",")
+    else:
+        print(name)
--- front_end/scouter_app/app/redux_stuff/currentScoutReducer.js
+++ front_end/scouter_app/app/redux_stuff/currentScoutReducer.js
@@ -1,14 +1,53 @@
 import { produce } from 'immer';
 import * as actionTypes from './actionTypes';
 
-const initialState = {
-    compName: "testComp",
-    roundName: "12test",
-    teamName: "7845t",
-    points: 7,
+const currentScoutInitialState = {
+    // metadata
+    compName: "",
+    matchNumber: 0,
+    teamNumber: 0,
+    scouterName: "",
+    // need to figure out how to do the below \/
+    //whenCaptured: Date.getUTCDate(),
+    scouterTeamNumber: 0,
+    driverStation: -1,
+    // autonomous
+    startPlace: -1,
+    ballsInUpperAuto: 0,
+    ballsInLowerAuto: 0,
+    ballsMissedAuto: 0,
+    passedLine: false,
+    ballsHumanShotAuto: false,
+    ballsHumanScoredAuto: false,
+    whichBallsCollected: [],
+    autoMalfunction: false,
+    autoFreeText: "",
+    // tele-op
+    ballsInUpperTele: 0,
+    ballsInLowerTele: 0,
+    ballsMissedTele: 0,
+    // end game
+    levelClimbed: 0,
+    climbSuccessful: true,
+    climbTime: 0,
+    // post game
+    defensiveDefenseLevel: -1,
+    offensiveDefenseLevel: -1,
+    wasDefendedLevel: -1,
+    shootingLocations: [],
+    collectingLocations: [],
+    goodTeamMateLevel: 4,
+    wasBroken: -1,
+    freeText: "",
+    generalImpression: 4,
+    // post game flags
+    robotNoFunction: false,
+    systemNoFunction: false,
 }
+// This part of the store handles the queue of ready oneScout to send
+// const scoutSendingQueue = { notEmpty };
 
-export default function currentScoutReducer(state = initialState, action) {
+export default function currentScoutReducer(state = currentScoutInitialState, action) {
     switch (action.type) {
         case actionTypes.COMP_NAME_SET:
             return produce(state, stateCopy => {
@@ -17,22 +56,24 @@ export default function currentScoutReducer(state = initialState, action) {
             });
         case actionTypes.ROUND_NAME_SET:
             return produce(state, stateCopy => {
-                stateCopy.roundName = action.payload.name;
+                stateCopy.matchNumber = action.payload.name;
                 return stateCopy;
             });
         case actionTypes.TEAM_NAME_SET:
             return produce(state, stateCopy => {
-                stateCopy.teamName = action.payload.name;
+                stateCopy.teamNumber = action.payload.name;
                 return stateCopy;
             });
         case actionTypes.POINTS_ADDED:
+            // ballsInUpperAuto is temp fix so code doesn't break - points is not generrally a part of this    
             return produce(state, stateCopy => {
-                stateCopy.points += action.payload.points;
+                stateCopy.ballsInUpperAuto += action.payload.points;
                 return stateCopy;
             });
         case actionTypes.POINTS_DEDUCTED:
+            // ballsInUpperAuto is temp fix so code doesn't break - points is not generrally a part of this    
             return produce(state, stateCopy => {
-                stateCopy.points -= action.payload.points;
+                stateCopy.ballsInUpperAuto -= action.payload.points;
                 return stateCopy;
             });
         default:
