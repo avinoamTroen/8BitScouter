edeab04c0c45c7c48f3bbacfb1651f3deca5ff3f
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-03-14 16:56:32 +0200

    added struct of out data

---
+++ back_end/servers/out_data.py
@@ -0,0 +1,26 @@
+# all functions in this module which return data from sql will return a
+# jsonified object which can be sent
+
+# remove default unuseful data like -1
+def remove_unused_values(data_dict):
+    pass
+
+# *********************************  "Expected" next round  ****************************************
+# return average (of relevant data) of scouts - a json
+def get_avg(data_dict, num_of_rounds):
+    pass
+
+# return min/max - a json
+
+
+# return time past adjusted score - a json
+
+# **********************************  Match expectation  *************************************
+# return
+
+
+
+# *************************************  Ranked Top Teams  ***************************************************
+
+# ********************************* Comp expectation ***************************************
+
