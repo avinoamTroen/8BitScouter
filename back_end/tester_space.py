9899c71eb5130975dd7389d93ed5cab8bdf36d65
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-05-25 16:38:46 +0300

    deleted get_single_scout func since it isn't used

--- back_end/tester_space.py
+++ back_end/tester_space.py
@@ -1,37 +1,23 @@
-paramNames = """id
-compName
-matchType
-matchNumber
-teamNumber
-scouterName
-whenCaptured
-scouterTeamNumber
-ballsInUpperAuto
-ballsInLowerAuto
-ballsMissedAuto
-passedLine
-ballsHumanShotAuto
-ballsHumanScoredAuto
-autoMalfunction
-autoFreeText
-ballsInUpperTele
-ballsInLowerTele
-ballsMissedTele
-levelClimbed
-climbSuccessful
-climbTime
-defensiveDefenseLevel
-offensiveDefenseLevel
-wasDefendedLevel
-goodTeamMateLevel
-wasBroken
-freeText
-generalImpression
-robotNoFunction
-systemNoFunction
-""".splitlines()
-c = 0
-for n in paramNames:
-    if n[0:2] != '//':
-        print(f"avg_dict['{n}'] = data_dict['{n}S']  # CHANGE_THIS", end="\n")
-        c += 1
+#python-docx
+from docx import *
+
+from os import walk
+document = Document()
+folder = "C:\\Users\\avino\\OneDrive\\Desktop\\8bit_scouting\\back_end\\servers\\"
+# "C:\Users\avino\OneDrive\Desktop\8bit_scouting\back_end\servers"
+filenames = next(walk(folder), (None, None, []))[2]  # [] if no file
+print(filenames)
+for file in filenames[::1]:
+    p = document.add_paragraph()
+    p.add_run(f'the file: {file}\n').bold = True
+    file = open(f'{folder}{file}')
+    #print(file.read())
+    try:
+        text = file.read()
+        p.add_run(text)
+        p.add_run('\n\n')
+        file.close()
+    except Exception as error:
+        print(file)
+
+document.save('demo.docx')
