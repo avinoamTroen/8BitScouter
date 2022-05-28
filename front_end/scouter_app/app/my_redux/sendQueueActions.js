b49292694d00fcc9b33e95dbc04a839fd8a7bb93
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-05-28 23:46:46 +0300

    fixed up redux of scouter + took off logs from networking

--- front_end/scouter_app/app/my_redux/sendQueueActions.js
+++ front_end/scouter_app/app/my_redux/sendQueueActions.js
@@ -1,12 +1,7 @@
 import * as actionTypes from './sendQueueActionTypes';
 
-// *** generic setter actions ***
-
 // metadata
 export function addQueue(newScout) {
-    console.log('starting addQueue - actions')
-    console.log(newScout)
-    console.log(' <<')
     return { type: actionTypes.Queue_ADDED, payload: { newScout: newScout } };
 }
 export function removeQueue() {
--- front_end/scouter_app/app/networking/sendScouts.js
+++ front_end/scouter_app/app/networking/sendScouts.js
@@ -7,22 +7,15 @@ function delay(time) {
 }
 
 export default async function sendScouts() {
-    console.log('starting sendSCoputs - send scouts')
-    console.log(store.getState().scoutSendingQueue.length)
-
     while (store.getState().scoutSendingQueue.length > 0) {
-        console.log('starting send')
         const scout = store.getState().scoutSendingQueue[0]
         res = await sendOneScout(scout);
         if (res.successful) {
             store.dispatch(removeQueue())
-            console.log('success')
         }
         else {
-            // needs improvement
             let s = 1000;
             await delay((s * 10))
-            console.log(res.msg)
         }
     }
 }
