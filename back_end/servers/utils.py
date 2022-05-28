8c12a0ca1a599335e709fb7d3e25f7679838a2fc
avinoamTroen <65117396+avinoamTroen@users.noreply.github.com>
2022-05-28 23:35:36 +0300

    deleted two outdated vars from utils and added some comments

--- back_end/servers/utils.py
+++ back_end/servers/utils.py
@@ -1,14 +1,16 @@
-DEV = True
+DEV = True  # if running in development env
 if DEV:
-    IPADDR = "0.0.0.0"  # i think this should work for everything so ill leave dev always true for now
+    IPADDR = "0.0.0.0"  # dev environment - goes to computers default
 else:
-    IPADDR = "184.72.229.230"
+    IPADDR = "184.72.229.230"  # server IP
+
+# ports for the different servers
 INPUT_SERVER_PORT = 3173
 OUTPUT_SERVER_PORT = 3713
-HOST_NAME = "localhost"
+
+# paths for different functions
 PATH_FOR_INPUT = '/input_server/one_scout/json'
-PATH_FOR_SCOUT_OUTPUT = '/output_server/one_scout/json'
 PATH_FOR_SCOUT_TEAM_OUTPUT = '/output_server/scout_team/json'
-AVG = '/avg'
+AVG = '/avg'  # to add to PATH_FOR_SCOUT_TEAM_OUTPUT if want avg (currently only option)
 
 PATH_FOR_TEAM_LIST_OUTPUT = '/output_server/team_list/json'
